37f04a65344f4d39eba986395e618f9b1de43d7f
TapurianMD <TapurianMD@gmail.com>
2016-11-27 04:44:48 -0800

    Create ProgrammerWay.h

---
+++ ProgrammerWay.h
@@ -0,0 +1,20 @@
+/* Copyright © 2016, Xerxes Langerudi. All rights reserved */
+
+#include <iostream>
+#include <string>
+
+#ifndef __Programmer_H__
+#define __Programmer_H__
+
+using namespace std;
+
+class Show
+{
+public:
+	void Menu();
+	void Item(int);
+	void PrintArray(short, string[]);
+};
+
+#endif
+
